eba9b2c1ee9b46456e59620d9ad5104e008eac54
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-08-15 12:29:26 +0200

    adds app to request

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ if (file_exists($maintenance)) {
 /** @var \Lemon\Kernel\Application $application */
 $application = include __DIR__.'/../init.php';
 
-$application->add(Request::class, Request::capture());
+$application->add(Request::class, Request::capture()->injectApplication($application));
 $application->alias('request', Request::class);
 
 $application->boot();
